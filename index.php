8c7928d79f2e31661cc7f39556a94300f2b74967
Vishesh1144 <your_email@example.com>
2024-10-16 12:02:38 +0530

    this is the test from vs code and vinit laptop

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
-<?php
+<!-- <?php
 $ser = "localhost";
 $usr = "root";
 $pass = "";
 
 $conn = mysqli_connect($ser,$usr,$pass)
- ?>
+ ?>\
+<?php -->
+    echo "This is the file i wan"
+?> 
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xammp Folder</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
+
+
